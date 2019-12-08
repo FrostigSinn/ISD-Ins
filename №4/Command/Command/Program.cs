6c806f404ff6d97c0e1f994d4b9874d1ec74de2d
Vladislav Podgrebelnyi <frostigsinn@gmail.com>
2019-12-08 21:45:03 +0200

    Added facade and command patterns

---
+++ №4/Command/Command/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    interface ICommand
+    {
+        void execude();
+        void undo();
+    }
+
+    class Programmer
+    {
+        public void startCoding()
+        {
+            Console.WriteLine("Programmer writes a program");
+        }
+
+        public void stopCoding()
+        {
+            Console.WriteLine("Programmer stopped work");
+        }
+    }
+
+    class CodeCommand : ICommand
+    {
+        Programmer programmer;
+
+        public CodeCommand(Programmer someProgrammer)
+        {
+            programmer = someProgrammer;
+        }
+
+        public void execude()
+        {
+            programmer.startCoding();
+        }
+
+        public void undo()
+        {
+            programmer.stopCoding();
+        }
+    }
+
+    class Tester
+    {
+        public void startTesting()
+        {
+            Console.WriteLine("Testing project");
+        }
+
+        public void stopTesting()
+        {
+            Console.WriteLine("Tester stopped work");
+        }
+    }
+
+
+    class TestCommand : ICommand
+    {
+        Tester tester;
+
+        public TestCommand(Tester someTester)
+        {
+            tester = someTester;
+        }
+
+        public void execude()
+        {
+            tester.startTesting();
+        }
+
+        public void undo()
+        {
+            tester.stopTesting();
+        }
+    }
+
+    class ProjectManager
+    {
+        List<ICommand> commands;
+        public void setCommand(List<ICommand> someCommands)
+        {
+            commands = someCommands;
+        }
+
+        public void startWork()
+        {
+            foreach(var command in commands)
+                command.execude();
+        }
+
+        public void stopWork()
+        {
+            foreach (var command in commands)
+                command.undo();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjectManager someManager = new ProjectManager();
+            Programmer someProgrammer = new Programmer();
+            Tester someTester = new Tester();
+
+            List<ICommand> commands = new List<ICommand>
+            {
+                new CodeCommand(someProgrammer),
+                new TestCommand(someTester)
+            };
+
+            someManager.setCommand(commands);
+
+            someManager.startWork();
+            someManager.stopWork();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ №4/Facade/Facade/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Musician
+    {
+        protected string Name { get; }
+
+        public Musician(string someName)
+        {
+            Name = someName;
+        }
+    }
+
+    class Vocalist : Musician
+    {
+        public Vocalist(string someName) : base(someName) { }
+
+        public void singCouplet(int coupletNumber)
+        {
+            Console.WriteLine("{0} singing {1} couplet of song", Name, coupletNumber);
+        }
+
+        public void singChorus()
+        {
+            Console.WriteLine("{0} singing chorus of song", Name);
+        }
+    }
+
+    class GuitarPlayer : Musician
+    {
+        public GuitarPlayer(string someName) : base(someName) { }
+
+        public void startPlaying()
+        {
+            Console.WriteLine("{0} start plaing guitar part", Name);
+        }
+
+        public void stopPlaying()
+        {
+            Console.WriteLine("{0} stop plaing guitar part", Name);
+        }
+
+        public void playSolo()
+        {
+            Console.WriteLine("{0} plaing solo part", Name);
+        }
+    }
+
+    class Drummer : Musician
+    {
+        public Drummer(string someName) : base(someName) { }
+
+        public void startPlaying()
+        {
+            Console.WriteLine("{0} start plaing drumms part", Name);
+        }
+
+        public void stopPlaying()
+        {
+            Console.WriteLine("{0} stop plaing drumms part", Name);
+        }
+    }
+
+    class MusicBand
+    {
+        Vocalist someVocalist = new Vocalist("Tom");
+        GuitarPlayer someGuitarPlayer = new GuitarPlayer("Ross");
+        Drummer someDrummer = new Drummer("Bob");
+
+        public void palySong()
+        {
+            someGuitarPlayer.startPlaying();
+            someDrummer.startPlaying();
+            someVocalist.singCouplet(1);
+            someVocalist.singChorus();
+            someGuitarPlayer.playSolo();
+            someVocalist.singCouplet(2);
+            someGuitarPlayer.stopPlaying();
+            someDrummer.stopPlaying();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicBand someBand = new MusicBand();
+            someBand.palySong();
+
+            Console.ReadLine();
+        }
+    }
+}
